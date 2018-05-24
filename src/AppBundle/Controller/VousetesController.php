ec26bbea23c3f40a69ee13b868296aae5c195dae
Unknown <max.garnier@live.fr>
2018-05-24 16:51:01 +0200

    update node + page vousetes

--- src/AppBundle/Controller/VousetesController.php
+++ src/AppBundle/Controller/VousetesController.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: garniermaxime
- * Date: 24/05/2018
- * Time: 10:53
- */
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class VousetesController extends Controller
+{
+    /**
+     * @Route("/vousetes", name="vousetes")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('vousetes/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
