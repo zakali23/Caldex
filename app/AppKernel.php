edf3b4bbe89886edb295bdf15e592badd2185667
Zowac <kevinvanderstracten@gmail.com>
2018-06-06 16:29:48 +0200

    Mise à jour du contenu de la page d'accueil, mise à jour du contenu de la page Vous-êtes, ajout du logo dans la barre de navigation, changement du couleur de la barre de navigation, mise à jour des informations dans le footer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
+            //new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
