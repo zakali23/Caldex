f31815f23f848b4c2922eb536da809dd30a3b96f
zakaria zekraoui <zakaria.zekraoui@gmail.com>
2018-06-11 15:46:31 +0200

    navbar ok

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ Encore
     .setPublicPath('/web')
     .addEntry('app', './assets/js/app.js')
     .addEntry('style', './assets/scss/main.scss')
+    .addEntry('header', './assets/scss/header.scss')
     .addEntry('entreprise', './assets/scss/entreprise.scss')
     .addEntry('login', './assets/scss/login.scss')
     .addEntry('main_admin', './assets/scss/main_admin.scss')
