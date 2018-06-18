1749bb55cb55e0fd591771bb44087c81aefadeb2
Unknown <max.garnier@live.fr>
2018-06-18 16:31:17 +0200

    refonte joli menu de zak
    avec l'aide désagréable de coralie

--- assets/js/header_admin.js
+++ assets/js/header_admin.js
@@ -1,10 +1,74 @@
-$(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-    $(".side-nav .collapse").on("hide.bs.collapse", function() {
-        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-right").addClass("fa-angle-down");
+
+//"use strict";
+
+var Dashboard = function () {
+    var global = {
+        tooltipOptions: {
+            placement: "right"
+        },
+        menuClass: ".c-menu"
+    };
+
+    var menuChangeActive = function menuChangeActive(el) {
+        var hasSubmenu = $(el).hasClass("has-submenu");
+        $(global.menuClass + " .is-active").removeClass("is-active");
+        $(el).addClass("is-active");
+
+        // if (hasSubmenu) {
+        //     $(el).find("ul").slideDown();
+        // }
+    };
+
+    var sidebarChangeWidth = function sidebarChangeWidth() {
+        var $menuItemsTitle = $("li .menu-item__title");
+
+        $("body").toggleClass("sidebar-is-reduced sidebar-is-expanded");
+        $(".hamburger-toggle").toggleClass("is-opened");
+
+        if ($("body").hasClass("sidebar-is-expanded")) {
+            $('[data-toggle="tooltip"]').tooltip("destroy");
+        } else {
+            $('[data-toggle="tooltip"]').tooltip(global.tooltipOptions);
+        }
+    };
+
+    return {
+        init: function init() {
+            $(".js-hamburger").on("click", sidebarChangeWidth);
+
+            $(".js-menu li").on("click", function (e) {
+                menuChangeActive(e.currentTarget);
+            });
+
+            $('[data-toggle="tooltip"]').tooltip(global.tooltipOptions);
+        }
+    };
+}();
+
+Dashboard.init();
+//# sourceURL=pen.js
+
+$(document).ready(function()
+{
+    $("li#GU").click(function() {
+
+        $("#Gestion-Utilisateurs").css({"display": "contents"});
+        $("#Gestion-Parc").css({"display": "none"});
+        $("#Gestion-Materiel").css({"display": "none"});
     });
-    $('.side-nav .collapse').on("show.bs.collapse", function() {
-        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-down").addClass("fa-angle-right");
+
+    $("li#GP").click(function() {
+
+        $("#Gestion-Utilisateurs").css({"display": "none"});
+        $("#Gestion-Parc").css({"display": "contents"});
+        $("#Gestion-Materiel").css({"display": "none"});
     });
-}) ;
-    
+
+    $("li#GM").click(function() {
+
+        $("#Gestion-Utilisateurs").css({"display": "none"});
+        $("#Gestion-Parc").css({"display": "none"});
+        $("#Gestion-Materiel").css({"display": "contents"});
+    });
+});
+
