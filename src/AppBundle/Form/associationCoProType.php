d1be38a6f022fd16be469192f9121e5b5f84949b
zakaria zekraoui <zakaria.zekraoui@gmail.com>
2018-07-25 15:52:34 +0200

    mise a jour les pages new

--- src/AppBundle/Form/associationCoProType.php
+++ src/AppBundle/Form/associationCoProType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,7 @@ class associationCoProType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name',TextType::class,array('label'=>'Nom'))
             ->add('adresse')
             ->add('codePostal')
             ->add('ville')
