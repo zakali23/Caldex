7b9bf568fe4c604a75950c4df6ff62251b25cefa
zakaria zekraoui <zakaria.zekraoui@gmail.com>
2018-07-26 11:40:01 +0200

    modification logo pdf ok

--- assets/js/ajaxPieces.js
+++ assets/js/ajaxPieces.js
@@ -21,7 +21,7 @@ function showRooms() {
                 "<button id='"+ value['id'] +"' type='submit' name='envoiImmeuble'>Piece :"+ value['nom'] +" </button>" + "</div></form>"+"<div class=\"list-group-horizontal\">"+
                 "<p class=\"list-group-item\"> Consommation <br><br><a href=\"consultation/lots/pdf\" target=\"_blank\" class=\"btn btn-primary\">Voir la consommation</a><br><br><a href=\"consultation/lots/dl\" class=\"btn btn-primary\">Télécharger la consommation</a></p>"+
                 "<p class=\"list-group-item\">Surface en m2: <br><br><br><span style='font-size: 20px;'>" + value['surface'] + "</span></p>"+
-                "<p class=\"list-group-item\">"+"</div>"+"</div>"+
+                "<p class=\"list-group-item\">Compteur :"+"</div>"+"</div>"+
                 "<p class='text-right'><button style='width: 10%' data-toggle=\"modal\" data-target='#myModalImgRoom"+ value['id'] +"' >Photo</button></p><hr class=\\\"my-5\\\">"+
 
                 "<div class='modal fade' id='myModalImgRoom"+ value['id'] +"' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>"+
